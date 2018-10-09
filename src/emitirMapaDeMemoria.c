9ef16e9a1bea9dee0d8ef942373061aec3014a35
Renan Clarindo Amorim <recam94@gmail.com>
2018-10-09 12:54:49 -0300

    começo do mapa de memoria

--- src/emitirMapaDeMemoria.c
+++ src/emitirMapaDeMemoria.c
@@ -1,6 +1,10 @@
 #include "montador.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MAPA_TAM 1024
+#define LINHA_TAM 5
 
 /*
     ---- Voce deve implementar essa função para a Parte 2! ----
@@ -9,8 +13,43 @@
         *  1 caso haja erro na montagem; (imprima o erro em stderr)
         *  0 caso não haja erro.
  */
+
+
+char *traduz_instr(Token tok){
+	if (!strcmp(tok.palavra, "ld") ) {
+
+	} 
+	else if () {
+
+	}
+	else if {
+
+	}
+}
+
+
+
+
 int emitirMapaDeMemoria()
 {
-    /* printf("Voce deve implementar essa função para a Parte 2!");*/
+    int i = 0;
+    int qtd_tokens = getNumberOfTokens();
+    char ***mapa = NULL;
+
+    mapa = malloc (MAPA_TAM * sizeof(char**));
+    for (int i = 0; i <MAPA_TAM; i++){
+    	mapa[i] = malloc(LINHA_TAM * sizeof(char*));
+    	for (int j = 0; j < LINHA_TAM; j++) {
+    		mapa[i][j] = NULL;
+    	} 
+    }
+
+
+    Token aux;
+    for (i = 0; i < qtd_tokens; i++) {
+    	aux = recuperaToken(i);
+    }
+
+
     return 0;
 }
